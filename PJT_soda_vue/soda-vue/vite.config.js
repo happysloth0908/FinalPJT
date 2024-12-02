2b4b1a5ce42a2dab1832419de7fd6b439827cde2
soun <sozzykim@naver.com>
2024-12-03 04:29:03 +0900

    배포 준비

--- PJT_soda_vue/soda-vue/vite.config.js
+++ PJT_soda_vue/soda-vue/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FinalPJT/',  // GitHub 저장소 이름으로 변경
   plugins: [
     vue(),
     vueDevTools(),
